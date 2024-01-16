53c30f47d2aeeb7ba59bc4f1cbd3ae3b2295837d
BastD29 <bastiendaniel9@gmail.com>
2024-01-16 12:14:28 +0100

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 connectDB();
 
 const port = process.env.PORT || 7000;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://portfolio-4kod.onrender.com",
+    methods: "GET",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 app.use(express.json());
 
